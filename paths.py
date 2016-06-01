a3ddafaaafe98eb7bde4949a32c5d0a566445d39
Yakir Abraham Reshef <yar2@loge.orchestra>
2016-06-01 15:51:48 -0400

    updated paths.py after creating fcor directory in folder structure

--- paths.py
+++ paths.py
@@ -4,16 +4,15 @@ from __future__ import print_function, division
 code = '/groups/price/yakir/py/'
 foreign = '/groups/price/yakir/foreign/'
 
-data = '/groups/price/yakir/data/'
-metadata = '/groups/price/yakir/metadata/'
+data = '/groups/price/yakir/fcor/data/'
 reference = data + 'reference/'
 genome_subsets = data + 'genome_subsets/'
 annotations = data + 'annot/'
 simsumstats = data + 'simsumstats/'
 
-results = '/groups/price/yakir/results/'
+results = '/groups/price/yakir/fcor/results/'
 
-metadata = '/groups/price/yakir/metadata/'
+metadata = '/groups/price/yakir/fcor/metadata/'
 architectures = metadata + 'architectures/'
 simulations = metadata + 'simulations/'
 experiments = metadata + 'experiments/'
